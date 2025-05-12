3245f0c56d76d3c9456e229b52cf036b40eea262
kacpro9 <sterkowiczkacper@gmail.com>
2025-05-12 20:58:45 +0200

    to nav done

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../views/Home";
+import Login from "../views/Login";
+import SignUp from "../views/SignUp";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+};
+export default AppRoutes;
